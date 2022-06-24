f576d13262074b016d3541830f11a873c9af764a
furkankirenci <kirencifurkan1@gmail.com>
2022-06-24 07:23:17 +0300

    Update

---
+++ src/LongestWord.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class LongestWord {
+    public static String LongestWord(String sen) {
+        // Coderbyte LongestWord soru cevabıdır.
+        // code goes here
+        // System.out.println(sen);
+
+        int len = sen.length();
+        String max = "", temp = "";
+        for (int i = 0; i < len; ++i) {
+            char c = sen.charAt(i);
+            int n = (int) c;
+            if ((n >= 97 && n <= 122) || (n >= 65 && n <= 90) || (n >= 48 & n <= 57)) {
+                temp = temp + sen.charAt(i);
+                if (i == len - 1) {
+                    if (max.length() < temp.length()) {
+                        max = temp;
+                        temp = "";
+                    }
+                }
+            } else {
+                if (max.length() < temp.length()) {
+                    max = temp;
+                }
+                temp = "";
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        // keep this function call here
+        Scanner a = new Scanner(System.in);
+        String s = "What is the longest word??";
+        System.out.print(LongestWord(s));
+    }
+}
---
+++ src/SifreCozucu.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class SifreCozucu {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("tahmin edilecek sayi max kac basamakli olsun : ");
+        int n = input.nextInt();
+        int number = (int) Math.floor(Math.random() * Math.pow(10, n));
+//        int number = 777;
+//        System.out.println(number);
+        boolean isCorrect = false;
+        int counter = 0;
+        while (isCorrect == false) {
+            int guess = guess();
+            counter++;
+            System.out.println(counter + " defa denedin.");
+            isCorrect = basamaklaraAyirma(number, guess, isCorrect, n);
+        }
+    }
+
+
+    static int guess() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayiyi tahmin ediniz : ");
+        int guess = input.nextInt();
+        return guess;
+    }
+
+
+    static boolean basamaklaraAyirma(int number, int guess, boolean isCorrect, int n) {
+
+        int[] guesses = new int[n];
+        int[] numbers = new int[n];
+        int add1 = 0;
+        int add2 = 0;
+        int correctPlaceCounter = 0;
+        int wrongPlaceCounter = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int a = (int) (guess / Math.pow(10, (n - i))) % 10;
+            guesses[add1++] = a;
+        }
+        for (int i = 1; i <= n; i++) {
+            int a = (int) (number / Math.pow(10, (n - i))) % 10;
+            numbers[add2++] = a;
+        }
+
+        for (int j = 0; j < guesses.length; j++) {
+            if (guesses[j] == numbers[j]) {
+                numbers[j] = 10;
+                guesses[j] = 10;
+                correctPlaceCounter++;
+            }
+        }
+
+        for (int j = 0; j < guesses.length; j++) {
+//            System.out.println(guesses[j]);
+            for (int k = 0; k < numbers.length; k++) {
+//                System.out.println(numbers[k]+" k bu");
+                if ((j != k) && (guesses[j] == numbers[k]) && (10 != guesses[j])) {
+                    wrongPlaceCounter++;
+//                    System.out.println(j+" "+k+" "+guesses[j]);
+                    numbers[k] = 10;
+                    break;
+                }
+            }
+        }
+
+        if (number == guess) {
+            System.out.println("Win");
+            isCorrect = true;
+        } else {
+            System.out.println(correctPlaceCounter + " dogru yer " + wrongPlaceCounter + " yanlis yer");
+        }
+        return isCorrect;
+    }
+}
